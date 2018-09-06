e8bc84d4b13b3ae730822a81187fb9b7b1955d5a
Matheus Duarte <mfduar8766@gmail.com>
2018-09-06 15:01:33 -0400

    Made changes to links.

--- src/Social.js
+++ src/Social.js
@@ -5,21 +5,21 @@ export default () => {
       <div className="container text-center">
           <div className="row justify-content-center">
               <div className="col-3 col-lg-3">
-                  <span>
+                  <span className="justify-content-center">
                       <a href="" rel="noopener noreferrer" target="_blank">
                           <i className="fa fa-facebook" aria-hidden="true" />
                       </a>
                   </span>
               </div>
               <div className="col-3 col-lg-3">
-                  <span>
+                  <span className="justify-content-center">
                       <a href="" rel="noopener noreferrer" target="_blank">
                           <i className="fa fa-twitter" aria-hidden="true" />
                       </a>
                   </span>
               </div>
               <div className="col-3 col-lg-3">
-                  <span>
+                  <span className="justify-content-center">
                       <a href="" rel="noopener noreferrer" target="_blank">
                           <i className="fa fa-instagram" aria-hidden="true" />
                       </a>
